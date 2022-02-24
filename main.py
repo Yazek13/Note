6ecf76f48f735fb99ef86928a3e85d8c6284e009
Yazek13 <fedorov_d_l@mail.ru>
2022-02-25 09:37:01 +1000

    Create program Note

---
+++ main.py
@@ -0,0 +1,90 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
+
+root = Tk()
+root.geometry('1000x500+800+300')
+root.title("Мое приложение для заметок")
+
+main_menu = Menu(root)
+root.config(menu=main_menu)
+
+
+def about_program():
+    """Кнопка (О программе)"""
+    messagebox.showinfo("About Notepad", "Для ЦФТ")
+
+
+def notepad_quit():
+    """Кнопа (Выход)"""
+    answer = messagebox.askokcancel("Выход", "Закрыть программу?")
+    if answer:
+        root.destroy()
+
+
+def open_file():
+    """Открыть фаил"""
+    file_path = filedialog.askopenfilename(title="Выбор файла",
+                                           filetypes=(("Текстовые документы (*.txt)", "*.txt"), ("Все файлы", "*.*")))
+    t.delete("1.0", END)
+    with open(file_path, encoding="utf-8") as f:
+        s = f.read()
+        t.insert("1.0", s)
+
+
+def save_file():
+    """Сохранить фаил"""
+    file_path = filedialog.asksaveasfilename(title="Выбор файла", filetypes=(("Текстовые документы (*.txt)", "*.txt"), ("Все файлы", "*.*")))
+
+    with open(file_path, "w", encoding="utf-8") as f:
+        text = t.get("1.0", END)
+        f.write(text)
+
+
+def change_theme(theme):
+    t['bg'] = theme_colors[theme]['text_bg']
+    t['fg'] = theme_colors[theme]['text_fg']
+    t['insertbackground'] = theme_colors[theme]['cursor']
+    t['selectbackground'] = theme_colors[theme]['select_bg']
+
+
+# File
+file_menu = Menu(main_menu, tearoff=0)
+file_menu.add_command(label="Открыть", command=open_file)
+file_menu.add_command(label="Сохранить", command=save_file)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=quit)
+main_menu.add_cascade(label="Файл", menu=file_menu)
+
+# Theme
+theme_menu = Menu(main_menu, tearoff=0)
+theme_menu_sub = Menu(theme_menu, tearoff=0)
+theme_menu_sub.add_command(label="Light Theme", command=lambda: change_theme('light'))
+theme_menu_sub.add_command(label="Dark Theme", command=lambda: change_theme('dark'))
+theme_menu.add_cascade(label="Оформление", menu=theme_menu_sub)
+theme_menu.add_command(label="О программе", command=about_program)
+main_menu.add_cascade(label="Разное", menu=theme_menu)
+
+f_text = Frame(root)
+f_text.pack(fill=BOTH, expand=1)
+
+theme_colors = {
+    "dark": {
+        "text_bg": "#343D46", "text_fg": "#fff", "cursor": "#EDA756", "select_bg": "#4E5A65"
+    },
+    "light": {
+        "text_bg": "#fff", "text_fg": "#000", "cursor": "#8000FF", "select_bg": "#777"
+    }
+}
+
+t = Text(f_text, bg=theme_colors['dark']['text_bg'], fg=theme_colors['dark']['text_fg'], padx=10, pady=10, wrap=WORD,
+         insertbackground=theme_colors['dark']['cursor'], selectbackground=theme_colors['dark']['select_bg'], width=30,
+         spacing3=10, font=("Courier New", 11))
+t.insert("1.0", "//Код писать здесь")
+t.pack(fill=BOTH, expand=1, side=LEFT)
+
+scroll = Scrollbar(f_text, command=t.yview)
+scroll.pack(fill=Y, side=LEFT)
+t.config(yscrollcommand=scroll.set)
+
+root.mainloop()
